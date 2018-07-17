7251f60db6eccab8306aede1f733f8f66254bb15
Tatyana Rodionva <ekorodionova@gmail.com>
2018-07-17 15:37:48 +0300

    Removed view methods

--- app/src/main/java/org/stepik/android/exams/core/presenter/contracts/TopicsListView.kt
+++ app/src/main/java/org/stepik/android/exams/core/presenter/contracts/TopicsListView.kt
@@ -5,11 +5,6 @@ import org.stepik.android.exams.graph.model.GraphData
 
 interface TopicsListView {
     fun showGraphData(graphData: GraphData)
-    fun onError(error: Errors)
-    fun hideRefreshView()
-    fun showRefreshView()
-    fun showErrorMessage(messageResId: Int)
-    fun hideErrorMessage()
     fun setState(state: State)
 
     sealed class State {
