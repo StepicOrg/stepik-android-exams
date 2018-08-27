4c98b996e74cbc686d947792a7f99642a2dc8383
Tatyana Rodionva <ekorodionova@gmail.com>
2018-08-27 18:43:58 +0300

    Add methods to AttemptRepository

--- app/src/main/java/org/stepik/android/exams/data/db/dao/AttemptEntitiyDao.kt
+++ app/src/main/java/org/stepik/android/exams/data/db/dao/AttemptEntitiyDao.kt
@@ -1,17 +1,19 @@
 package org.stepik.android.exams.data.db.dao
 
 import android.arch.persistence.room.*
+import io.reactivex.Completable
+import io.reactivex.Maybe
 import org.stepik.android.exams.data.db.entity.AttemptEntitiy
 
 interface AttemptEntitiyDao {
     @Query("SELECT * FROM AttemptEntitiy WHERE id = :id")
-    fun findAttemptById(id: Long): AttemptEntitiy
+    fun findAttemptById(id: Long): Maybe<AttemptEntitiy>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAttempt(attemptEntitiy: AttemptEntitiy)
+    fun insertAttempt(attemptEntitiy: AttemptEntitiy) : Completable
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updateAttempt(attemptEntitiy: AttemptEntitiy)
+    fun updateAttempt(attemptEntitiy: AttemptEntitiy) : Completable
 
     @Delete
     fun deleteAttempt(attemptEntitiy: AttemptEntitiy)
--- app/src/main/java/org/stepik/android/exams/data/repository/AttemptRepository.kt
+++ app/src/main/java/org/stepik/android/exams/data/repository/AttemptRepository.kt
@@ -1,12 +1,39 @@
 package org.stepik.android.exams.data.repository
 
+import org.stepik.android.exams.api.StepicRestService
 import org.stepik.android.exams.data.db.dao.AttemptEntitiyDao
+import org.stepik.android.exams.data.db.entity.AttemptEntitiy
+import org.stepik.android.exams.data.preference.SharedPreferenceHelper
+import org.stepik.android.exams.web.AttemptRequest
+import org.stepik.android.model.Step
 import javax.inject.Inject
 
 class AttemptRepository
 @Inject
 constructor(
-        private val attemptEntitiyDao: AttemptEntitiyDao
+        private val attemptEntitiyDao: AttemptEntitiyDao,
+        private var stepicRestService: StepicRestService,
+        private var sharedPreferenceHelper: SharedPreferenceHelper
 ) {
 
+    fun createAttempt(attempt: AttemptEntitiy) =
+            attemptEntitiyDao.insertAttempt(attempt)
+
+    fun updateAttempt(attempt: AttemptEntitiy) =
+            attemptEntitiyDao.updateAttempt(attempt)
+
+    fun findAttemptInDb(attemptId: Long) =
+            attemptEntitiyDao.findAttemptById(attemptId)
+
+    fun loadExistingAttempts(step: Step) =
+            stepicRestService
+                    .getExistingAttempts(step.id, sharedPreferenceHelper.getCurrentUserId() ?: 0)
+                    .filter { it.attempts.isNotEmpty() }
+                    .map { it.attempts.first() }
+
+    fun createNewAttempt(step: Step) =
+            stepicRestService
+                    .createNewAttempt(AttemptRequest(step.id))
+                    .filter { it.attempts.isNotEmpty() }
+                    .map { it.attempts.first() }
 }
