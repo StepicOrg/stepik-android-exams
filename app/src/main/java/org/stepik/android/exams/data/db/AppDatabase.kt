d873446e701f818fc5ae45c50529506e8f503f81
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-08-28 12:32:08 +0300

    remove attempt dao

--- app/src/main/java/org/stepik/android/exams/data/db/AppDatabase.kt
+++ app/src/main/java/org/stepik/android/exams/data/db/AppDatabase.kt
@@ -10,7 +10,6 @@ import org.stepik.android.exams.data.db.dao.*
 import org.stepik.android.exams.data.db.data.LessonInfo
 import org.stepik.android.exams.data.db.data.StepInfo
 import org.stepik.android.exams.data.db.data.TopicInfo
-import org.stepik.android.exams.data.db.entity.AttemptEntity
 import org.stepik.android.exams.data.db.entity.SubmissionEntity
 
 @Database(
@@ -18,7 +17,6 @@ import org.stepik.android.exams.data.db.entity.SubmissionEntity
             StepInfo::class,
             LessonInfo::class,
             TopicInfo::class,
-            AttemptEntity::class,
             SubmissionEntity::class
         ],
         version = 1,
@@ -29,7 +27,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun stepDao(): StepDao
     abstract fun navigationDao(): LessonDao
     abstract fun topicDao(): TopicDao
-    abstract fun attemptEntityDao() : AttemptEntitiyDao
     abstract fun submissionEntityDao() : SubmissionEntityDao
 
     companion object {
