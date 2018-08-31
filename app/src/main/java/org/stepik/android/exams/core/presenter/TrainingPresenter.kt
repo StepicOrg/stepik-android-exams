00ddd2c24fbc528d38e342d0663bc71903d29e91
Tatyana <ekorodionova@gmail.com>
2018-09-01 01:13:12 +0300

    Override attachView in TrainingPresenter

--- app/src/main/java/org/stepik/android/exams/core/presenter/TrainingPresenter.kt
+++ app/src/main/java/org/stepik/android/exams/core/presenter/TrainingPresenter.kt
@@ -2,7 +2,9 @@ package org.stepik.android.exams.core.presenter
 
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
 import org.stepik.android.exams.core.presenter.contracts.TrainingView
+import org.stepik.android.exams.data.model.LessonType
 import org.stepik.android.exams.data.repository.StepsRepository
 import org.stepik.android.exams.di.qualifiers.BackgroundScheduler
 import org.stepik.android.exams.di.qualifiers.MainScheduler
@@ -19,6 +21,9 @@ constructor(
         private val graph: Graph<String>,
         private val stepsRepository: StepsRepository
 ) : PresenterBase<TrainingView>() {
+    private val compositeDisposable = CompositeDisposable()
+    private var theoryLessons : List<LessonType.Theory> = listOf()
+    private var practiceLessons : List<LessonType.Practice> = listOf()
     val topicsList = graph.getAllTopics()
 
     init {
@@ -27,25 +32,38 @@ constructor(
     }
 
     private fun loadAllTheoryLessons() {
-        Observable.merge(topicsList.map { stepsRepository.loadTheoryLesson(it) })
+        compositeDisposable.add(Observable.merge(topicsList.map { stepsRepository.loadTheoryLesson(it) })
                 .subscribeOn(backgroundScheduler)
                 .observeOn(mainScheduler)
                 .toList()
-                .subscribe({
-                    view?.showTheoryLessons(it)
-                }, {})
+                .subscribe({ lessons ->
+                    theoryLessons = lessons
+                    view?.showTheoryLessons(lessons)
+                }, {}))
     }
 
     private fun loadAllPracticeLessons(){
-        Observable.merge(topicsList.map { stepsRepository.getPracticeCoursesId(it).toObservable() })
+        compositeDisposable.add(Observable.merge(topicsList.map { stepsRepository.getPracticeCoursesId(it).toObservable() })
                 .subscribeOn(backgroundScheduler)
                 .observeOn(mainScheduler)
                 .toList()
-                .subscribe({
-                    view?.showPracticeLessons(it)
-                }, {})
+                .subscribe({lessons ->
+                    practiceLessons = lessons
+                    view?.showPracticeLessons(lessons)
+                }, {}))
+    }
+
+    override fun attachView(view: TrainingView) {
+        super.attachView(view)
+        if (theoryLessons.isNotEmpty()){
+            view.showTheoryLessons(theoryLessons)
+        }
+        if (practiceLessons.isNotEmpty()){
+            view.showPracticeLessons(practiceLessons)
+        }
     }
 
     override fun destroy() {
+        compositeDisposable.clear()
     }
 }
